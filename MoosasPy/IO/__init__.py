4457c46dded3f9724a662547bd545cb7b75ed43b
UmikoXiao <1.47482094e+08+UmikoXiao@users.noreply.github.com>
2026-06-07 07:20:09 +0000

    Add RDF-gbXML roundtrip conversion utilities

--- MoosasPy/IO/__init__.py
+++ MoosasPy/IO/__init__.py
@@ -4,6 +4,8 @@ from ._graph import buildGraph, writeGraph
 from ._xml import writeXml,loadXml
 from ._json import writeJson, writeGeojson
 from ._rdf import writeRDF, loadRDF
+from ._gbxml import convert_rdf_to_gbxml, convert_gbxml_to_rdf, rdf_to_gbxml, gbxml_to_rdf
 from ._idf import writeIDF, IDFtoGeo, IDFtoXml, readIDF
 from . import _idf
 from .preprocess import *
+
