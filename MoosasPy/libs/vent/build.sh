3acf9ea0e48d1f0d0622fbae0523c447c73d7991
Romeo-Leeyh <1905516054@qq.com>
2026-08-28 11:48:11 -0700

    refactor: remove obsolete AFN and triangulate binaries and source files

--- MoosasPy/libs/vent/build.sh
+++ MoosasPy/libs/vent/build.sh
@@ -2,6 +2,3 @@
 set -euo pipefail
 
 go build -o MoosasAFN MoosasAFN.go
-go build -o ./afn/afn afn.go
-go build -o ./mkdir/mkdir ./mkdir/mkdir.go
-go build -o ./triangulate/triangulate ./triangulate/triangulate.go
